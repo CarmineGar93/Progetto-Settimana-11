f03791d802eff2ee5a721b0cec72468ea5c7ea7d
Carmy93 <carmygargiulo45@gmail.com>
2024-09-06 11:31:46 +0200

    main

--- spotify-clone/src/components/Mainfy.jsx
+++ spotify-clone/src/components/Mainfy.jsx
@@ -1,10 +1,24 @@
 import { Col, Row } from "react-bootstrap";
+import Sectionfy from "./Sectionfy";
 
 function Mainfy() {
     return (
-        <Row>
-            <Col xs={9} lg={11}></Col>
-        </Row>
+        <>
+            <Row>
+                <Col xs={9} lg={11} className="mainLinks d-none d-md-flex">
+                    <a href="#a">TRENDING</a>
+                    <a href="#b">PODCAST</a>
+                    <a href="#c">MOODS AND GENRES</a>
+                    <a href="#d">NEW RELEASES</a>
+                    <a href="#e">DISCOVER</a>
+                </Col>
+            </Row>
+            <Row>
+                <Sectionfy title='Rock Classics' search='queen' />
+                <Sectionfy title='Pop Classics' search='katyperry' />
+                <Sectionfy title='HipHop Classics' search='eminem' />
+            </Row>
+        </>
     )
 }
 
