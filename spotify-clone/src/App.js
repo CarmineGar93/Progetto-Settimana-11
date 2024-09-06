a99adbcb732ec484a7829ce2b4def63f3fb19cbd
Carmy93 <carmygargiulo45@gmail.com>
2024-09-06 10:50:20 +0200

    navbar

--- spotify-clone/src/App.js
+++ spotify-clone/src/App.js
@@ -1,13 +1,20 @@
-import {Container, Row} from 'react-bootstrap'
+import {Container, Row, Col} from 'react-bootstrap'
 import 'bootstrap/dist/css/bootstrap.min.css'
 import {BrowserRouter, Routes, Route} from 'react-router-dom'
 import './App.css';
+import Navbarfy from './components/Navbarfy';
 
 function App() {
   return (
    <BrowserRouter>
       <Container fluid>
-
+        <Row>
+          <Col xs={2}>
+            <Navbarfy />
+          </Col>
+          <Col xs={12} md={9} className='offset-md-3 main-page'>
+          </Col>
+        </Row>
       </Container>
    </BrowserRouter>
   );
---
+++ spotify-clone/src/components/Navbarfy.jsx
@@ -0,0 +1,51 @@
+import { Container, InputGroup, Button, Form, Navbar, Nav } from 'react-bootstrap'
+import house from '../house.svg'
+import book from '../book.svg'
+
+function Navbarfy() {
+    return (
+        <Navbar expand="md" className="fixed-left justify-content-between">
+            <Container className='flex-column align-items-start'>
+                <Navbar.Brand href="#home">
+                    <img
+                        src="/logo.png"
+                        alt="Spotify Logo"
+                        width="131"
+                        height="40" />
+                </Navbar.Brand>
+                <Navbar.Toggle aria-controls="basic-navbar-nav" />
+                <Navbar.Collapse id="basic-navbar-nav">
+                    <Nav className='flex-column'>
+                        <Nav.Link href="#home" className='d-flex flex align-items-center mb-2'>
+                            <img alt='' src={house}></img> &nbsp; Home
+                        </Nav.Link>
+                        <Nav.Link href="#home" className='d-flex align-items-center mb-2'>
+                            <img alt='' src={book}></img> &nbsp; Library
+                        </Nav.Link>
+                        <InputGroup className="mb-3">
+                            <Form.Control
+                                placeholder="Search"
+                                aria-label="Search"
+                                aria-describedby="basic-addon2"
+                            />
+                            <Button variant="outline-secondary" id="button-addon2">
+                                Go
+                            </Button>
+                        </InputGroup>
+                    </Nav>
+                </Navbar.Collapse>
+            </Container>
+            <div class="d-flex flex-column align-items-center w-100">
+                <Button className='signup-btn'>Sign Up</Button>
+                <Button className="login-btn" type="button">Login</Button>
+                <div className='nav-btn'>
+                    <a href="#home">Cookie Policy</a> |
+                    <a href="#home"> Privacy</a>
+                </div>
+
+            </div>
+        </Navbar>
+    )
+}
+
+export default Navbarfy
